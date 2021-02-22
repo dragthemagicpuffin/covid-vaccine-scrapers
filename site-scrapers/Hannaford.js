6f6673f2109d324ab172559b6e56b0eb9e9c75de
Zach Peyton <zachary.peyton@gmail.com>
2021-02-22 11:57:00 -0500

    Hannaford - save site body innerHTML to s3

--- site-scrapers/Hannaford.js
+++ site-scrapers/Hannaford.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+const AWS = require("aws-sdk");
 const sites = require("../data/sites.json");
 
 const noAppointmentMatchString = "no locations with available appointments";
@@ -63,10 +65,77 @@ async function ScrapeWebsiteData(browser) {
                 const filename =
                     "hannaford-zip-" + loc.zip + "-date-" + today + ".png";
                 await page.screenshot({ path: filename });
+                await saveBodyHtml(page);
             }
             results[loc.zip] = result;
         }
     }
 
     return results;
+}
+
+async function writeFile(filename, data) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(filename, data, (err) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve();
+            }
+        });
+    });
+}
+
+async function saveBodyHtml(page) {
+    // It appears puppeteer does not a generic page.waitForNetworkIdle
+    // so for now we just assume all XHR requests will complete within
+    // a reasonable timeframe, for now 10s.
+    await new Promise((resolve) => {
+        setTimeout(function () {
+            resolve("done waiting");
+        }, 1000);
+    });
+
+    const body = await page.$("body");
+    const html = await body.evaluate((node) => node.outerHTML);
+    const timestamp =
+        new Date().toISOString().substring(0, 16).replace(":", "") + "Z";
+    const fileName = ["hannaford-", timestamp, ".html"].join("");
+    await writeFile(fileName, html);
+    await uploadFileToS3("debug", fileName);
+    return fileName;
+}
+
+async function uploadFileToS3(bucketDir, fileName) {
+    const fileContents = await new Promise((resolve, reject) => {
+        fs.readFile(fileName, "utf-8", async (err, data) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(data);
+            }
+        });
+    });
+
+    const s3 = new AWS.S3({
+        accessKeyId: process.env.AWSACCESSKEYID,
+        secretAccessKey: process.env.AWSSECRETACCESSKEY,
+    });
+
+    const s3Key = [bucketDir, fileName].join("/");
+    const params = {
+        Bucket: process.env.AWSS3BUCKETNAME,
+        Key: s3Key,
+        Body: fileContents,
+    };
+
+    await new Promise((resolve, reject) => {
+        s3.upload(params, function (err, data) {
+            if (err) {
+                reject(err);
+            }
+            console.log(`File uploaded successfully. ${data.Location}`);
+            resolve(data);
+        });
+    });
 }
